9d8c9f28e6bb12b599854122f0c66d38d71dbc90
melisa-abuin <melisa.abuin@gmail.com>
2023-05-01 00:16:36 +0200

    refactor #24: delete unused files

--- src/components/map/index.tsx
+++ src/components/map/index.tsx
@@ -2,21 +2,21 @@ import { MapContainer, TileLayer } from 'react-leaflet'
 import { LocationMarker } from './components/locationMarker'
 import { SearchBox } from './components/searchBox'
 
-export const Map = () => {
-  return (
-    <MapContainer
-      center={[51.505, -0.09]}
-      minZoom={2}
-      scrollWheelZoom={true}
-      style={{ height: '100vh', width: '100vw' }}
-      zoom={4}
-    >
-      <TileLayer
-        url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-      />
-      <SearchBox />
-      <LocationMarker />
-    </MapContainer>
-  )
-}
+export const Map = () => (
+  <MapContainer
+    center={[51.505, -0.09]}
+    minZoom={2}
+    scrollWheelZoom={true}
+    style={{ height: '100vh', width: '100vw' }}
+    zoom={4}
+  >
+    <TileLayer
+      url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+    />
+
+    <SearchBox />
+
+    <LocationMarker />
+  </MapContainer>
+)
