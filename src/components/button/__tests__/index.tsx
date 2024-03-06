42225cbd7636ba2baf482d8237d60aca44c7e95d
melisa-abuin <melisa.abuin@gmail.com>
2024-03-06 22:44:48 +0100

    feature #10: add tests for button

---
+++ src/components/button/__tests__/index.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react'
+import { Button } from '..'
+import userEvent from '@testing-library/user-event'
+
+describe('Button', () => {
+  it('calls onclick function correctly', async () => {
+    const onClickMock = jest.fn()
+    render(<Button onClick={onClickMock}>test</Button>)
+
+    await userEvent.click(screen.getByText('test'))
+
+    expect(onClickMock).toHaveBeenCalled()
+  })
+})
