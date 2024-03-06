92efc891ee61a41b183f590ec76f51d607e69f99
melisa-abuin <melisa.abuin@gmail.com>
2024-03-06 22:44:26 +0100

    feature #10: add modal status

--- src/components/map/index.tsx
+++ src/components/map/index.tsx
@@ -5,34 +5,52 @@ import type { Location } from '@/interfaces/location'
 import { LocationMarker } from './components/locationMarker'
 import { AddPlaceButton } from './components/addPlaceButton'
 import { Modal } from '../modal'
+import { useState } from 'react'
 
-export const Map = ({ locations }: { locations: Location[] }) => (
-  <>
-    <MapContainer
-      center={[51.505, -0.09]}
-      minZoom={2}
-      scrollWheelZoom={true}
-      style={{ height: '100vh', width: '100vw' }}
-      zoom={4}
-    >
-      <TileLayer
-        url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-      />
+export const Map = ({ locations }: { locations: Location[] }) => {
+  const [modalState, setModalState] = useState(false)
 
-      <SearchBox />
+  const openModal = () => {
+    setModalState(true)
+  }
 
-      <LocationClickMarker />
+  const closeModal = () => {
+    setModalState(false)
+  }
 
-      {locations.map(({ id, xCoordinate, yCoordinate }) => (
-        <LocationMarker
-          key={id}
-          xCoordinate={xCoordinate}
-          yCoordinate={yCoordinate}
+  return (
+    <>
+      <MapContainer
+        center={[51.505, -0.09]}
+        minZoom={2}
+        scrollWheelZoom={true}
+        style={{ height: '100vh', width: '100vw' }}
+        zoom={4}
+      >
+        <TileLayer
+          url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
         />
-      ))}
-    </MapContainer>
-    <AddPlaceButton />
-    <Modal />
-  </>
-)
+
+        <SearchBox />
+
+        <LocationClickMarker />
+
+        {locations.map(({ id, xCoordinate, yCoordinate }) => (
+          <LocationMarker
+            key={id}
+            xCoordinate={xCoordinate}
+            yCoordinate={yCoordinate}
+          />
+        ))}
+      </MapContainer>
+      <AddPlaceButton onClick={openModal} />
+      <Modal
+        modalState={modalState}
+        onCrossClick={closeModal}
+        onLeftButtonClick={closeModal}
+        onRightButtonClick={closeModal}
+      />
+    </>
+  )
+}
--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -3,34 +3,62 @@ import { Input } from '../input'
 import styles from './modal.module.css'
 import Image from 'next/image'
 
-export const Modal = () => (
-  <div aria-labelledby="modalTitle" className={styles.modal} role="dialog">
-    <div className={styles.header}>
-      <div>
-        <h2 className={styles.title} id="modalTitle">
-          Add Place
-        </h2>
-        <p className={styles.subtitle}>To save a new memory</p>
+interface Props {
+  modalState: boolean
+  onCrossClick: () => void
+  onLeftButtonClick: () => void
+  onRightButtonClick: () => void
+}
+
+export const Modal = ({
+  modalState,
+  onCrossClick,
+  onLeftButtonClick,
+  onRightButtonClick,
+}: Props) => {
+  if (!modalState) {
+    return null
+  }
+
+  return (
+    <div aria-labelledby="modalTitle" className={styles.modal} role="dialog">
+      <div className={styles.header}>
+        <div>
+          <h2 className={styles.title} id="modalTitle">
+            Add Place
+          </h2>
+          <p className={styles.subtitle}>To save a new memory</p>
+        </div>
+        <Image
+          alt="Close modal"
+          height={15}
+          onClick={onCrossClick}
+          src="/close-icon.png"
+          width={15}
+        />
       </div>
-      <Image alt="Close modal" height={15} src="/close-icon.png" width={15} />
+      <div className={styles.content}>
+        <form>
+          <div className={styles.input}>
+            <Input type="text" id="name" name="name" required />
+          </div>
+          <div className={styles.input}>
+            <Input id="description" isTextArea name="description" />
+          </div>
+        </form>
+      </div>
+      <div className={styles.buttonGroup}>
+        <Button
+          borders="squared"
+          onClick={onLeftButtonClick}
+          variant="secondary"
+        >
+          Cancel
+        </Button>
+        <Button borders="squared" onClick={onRightButtonClick}>
+          Submit
+        </Button>
+      </div>
     </div>
-    <div className={styles.content}>
-      <form>
-        <div className={styles.input}>
-          <Input type="text" id="name" name="name" required />
-        </div>
-        <div className={styles.input}>
-          <Input id="description" isTextArea name="description" />
-        </div>
-      </form>
-    </div>
-    <div className={styles.buttonGroup}>
-      <Button borders="squared" onClick={() => {}} variant="secondary">
-        Cancel
-      </Button>
-      <Button borders="squared" onClick={() => {}}>
-        Submit
-      </Button>
-    </div>
-  </div>
-)
+  )
+}
